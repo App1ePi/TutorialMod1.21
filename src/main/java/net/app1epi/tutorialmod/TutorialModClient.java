b52543e511f9ec868edce40637fb4efb2949bacc
Erbium <102967870+App1ePi@users.noreply.github.com>
2024-10-21 10:55:52 +0100

    Initial commit

---
+++ src/main/java/net/app1epi/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.app1epi.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
